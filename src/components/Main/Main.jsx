37b5fdbd067128b1ef5441b4d84043dd9ec1c1ec
KimYevgeniy <kimeval@gmail.com>
2023-05-17 22:14:08 +0600

    fixed bugs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,47 +2,21 @@ import React, { useState } from 'react'
 import Modal from '../Modal/Modal'
 import { v4 as uuidv4 } from 'uuid'
 import './Main.css'
-import trash from './images/trash.png';
-import vector from './images/vector.png';
+import SingleToDo from './SingleToDo';
 
 export default function Main() {
   
     const [activePage, setActivePage] = useState('To Do')
 
     const [toDoList, setToDoList] = useState([
-        // {text: 'Do smth', id: uuidv4(), status: 'To Do'},
-        // {text: 'Other stuff', id: uuidv4(), status: 'To Do'},
-        // {text: 'Trash stuff', id: uuidv4(), status: 'Trash'},
-        // {text: 'Done stuff', id: uuidv4(), status: 'Done'},
-        // {text: 'Trash stuff', id: uuidv4(), status: 'Trash'}
+        {text: 'Do smth', id: uuidv4(), status: 'To Do'},
+        {text: 'Other stuff', id: uuidv4(), status: 'To Do'},
+        {text: 'Trash stuff', id: uuidv4(), status: 'Trash'},
+        {text: 'Done stuff', id: uuidv4(), status: 'Done'},
+        {text: 'Trash stuff', id: uuidv4(), status: 'Trash'}
     ])
 
     const activeList = toDoList.filter((item) => item.status === activePage)
-
-    const [isModalActive, setIsModalActive] = useState(false)
-
-    const toggleModal = () => {
-        setIsModalActive(!isModalActive)
-        console.log(isModalActive)
-    }
-    
-    const changeStatusDone = (item) => {
-        item.status = 'Done'
-    }
-    const changeStatusToDo = (item) => {
-        item.status = 'To Do'
-    }
-    const changeStatusTrash = (item) => {
-        item.status = 'Trash'
-    }
-
-    const changeText = () => {
-        if (activePage === 'To Do' || activePage === 'Done') {
-            return 'Move To Trash'
-        } else {
-            return 'Delete Forever'
-        }
-    }
     
     return (
         <>
@@ -61,27 +35,9 @@ export default function Main() {
             
             {toDoList.length === 0 && (<p className='inter500-16'>Look's like You got nothing To Do! Click on plus button to add a task</p>)}
 
-            <ul>
-                {activeList.map((item) => {
-                    return <li key={item.id} className='inter500-16'> <input type="checkbox" onChange={toggleModal} /> {item.text} </li>
-                })}
-            </ul>
-            
-            {isModalActive && (
-                    <div className="list-modal">
-                        <div className="overlay">
-                            <div className="options">
-                               <div className='options1'>
-                                    <img src={trash} className='icon'/>
-                                    <p className='inter500-14'> {changeText()}</p>
-                               </div>
-                               <div className='options1'>
-                                    <img src={vector} className='icon'/>
-                                    <p className='inter500-14'>Move Back To To Do</p>
-                               </div>
-                            </div>
-                        </div>
-                    </div>)}
+            {activeList.map((item) => 
+                <SingleToDo key={item.id} item={item} toDoList={toDoList} setToDoList={setToDoList} activePage={activePage} />
+            )}
         </>
   )
 }
---
+++ src/components/Main/SingleToDo.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import unchecked from './images/unchecked-icon.png';
+import dots from './images/3dots.png';
+import check from './images/check-icon.png';
+import trash from './images/trash.png';
+import vector from './images/vector.png';
+
+export default function SingleToDo ({ item, toDoList, setToDoList, activePage }) {
+    
+    const changeStatusToTrash = (id) => {
+        const itemToTrash = toDoList.find((item) => item.id === id)
+        itemToTrash.status = 'Trash'
+        const trashToDoList = toDoList.filter((item) => item.id !== id)
+        setToDoList([...trashToDoList, itemToTrash])
+    }
+
+    const changeStatusToDone = (id) => {
+        const itemToDone = toDoList.find((item) => item.id === id)
+        itemToDone.status = 'Done'
+        const doneToDoList = toDoList.filter((item) => item.id !== id)
+        setToDoList([...doneToDoList, itemToDone])
+    }
+
+    const changeStatusToToDo = (id) => {
+        const itemToToDo = toDoList.find((item) => item.id === id)
+        itemToToDo.status = 'To Do'
+        const toDoToDoList = toDoList.filter((item) => item.id !== id)
+        setToDoList([...toDoToDoList, itemToToDo])
+    }
+
+    const [isModalActive, setIsModalActive] = useState(false)
+    const toggleModal = () => {
+        setIsModalActive(!isModalActive)
+    }
+
+    const [isToDoModalActive, setIsToDoModalActive] = useState(false)
+    const toggleToDoModal = () => {
+        setIsToDoModalActive(!isModalActive)
+    }
+
+    const changeText = () => {
+        if (activePage === 'To Do' || activePage === 'Done') {
+            return 'Move To Trash'
+        } else {
+            return 'Delete Forever'
+        }
+    }
+
+    const changeAction = () => {
+        if (activePage === 'To Do') {
+            changeStatusToDone(item.id)
+        } else {
+            toggleModal()
+        }
+    }
+
+    return (
+        <>
+            <div className='singletodo'> 
+                <img src={dots} className='icon-clickable' onClick={ activePage === 'To Do' ? toggleToDoModal : toggleModal} />
+                <img src={item.status === 'Done' ? check : unchecked} className='icon' onClick={() => changeAction()}/>
+                <p className={item.status === 'Done' ? 'inter500-16-line' : 'inter500-16'}>{item.text}</p>
+            </div> 
+
+            {isToDoModalActive && (
+                    <div className="list-modal-todo">
+                        <div className="overlay">
+                            <div className="options">
+                               <div className='options1' onClick={() => changeStatusToTrash(item.id)}>
+                                    <img src={trash} className='icon'/>
+                                    <p className='inter500-14'> Move To Trash</p>
+                               </div>
+                            </div>
+                        </div>
+                    </div>)}
+
+            {isModalActive && (
+                    <div className="list-modal">
+                        <div className="overlay">
+                            <div className="options">
+                               <div className='options1' onClick={() => changeStatusToTrash(item.id)}>
+                                    <img src={trash} className='icon'/>
+                                    <p className='inter500-14'> {changeText()}</p>
+                               </div>
+                               <div className='options1' onClick={() => changeStatusToToDo(item.id)}>
+                                    <img src={vector} className='icon'/>
+                                    <p className='inter500-14'>Move Back To To Do</p>
+                               </div>
+                            </div>
+                        </div>
+                    </div>)}
+        </>
+    )
+}
